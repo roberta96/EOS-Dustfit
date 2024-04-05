9898c304980f3b9976c7efc16f851932d65fbbd8
Roberta Tripodi <65497179+roberta96@users.noreply.github.com>
2024-04-05 11:09:05 +0200

    Add files via upload

---
+++ colddust_sed_models/setup.py
@@ -0,0 +1,20 @@
+import io
+import os
+import sys
+from setuptools import setup
+
+setup(name="CDSED_modelling", 
+    version="1.0.6",
+    author="R.Tripodi",
+    author_email="robertatripodi.96@gmail.com",
+    description="Package for cold dust SED modelling",
+    url="<https://github.com/roberta96/Cold_sed_models>",
+    packages=["cdsed_modelling"]
+)
+
+'''packages=setuptools.find_packages(),
+classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",],
+python_requires='>=3.6','''
